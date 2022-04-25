f5317857161ccd588c7682936e89dafadac91880
Pawan Khatri <pawan.khatri@outlook.com>
2022-04-25 12:47:49 +0530

    Delete Folder.py

--- Folder.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-
-class NewFolder :
-    def __int__(self,name) :
-        self.subFolder = []
-        self.name = name
-        self.upload_time = datetime.now.strftime("%H:%M:%S %B %d, %Y")
